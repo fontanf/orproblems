488d9fbf56ee32e4e25825d94d52bf1001a64ca4
Florian Fontan <dev@florian-fontan.fr>
2023-04-16 11:48:01 +0200

    Update travelingsalesman

--- orproblems/travelingsalesman.hpp
+++ orproblems/travelingsalesman.hpp
@@ -142,6 +142,7 @@ public:
         if (verbose >= 1) {
             os << "Number of vertices:  " << number_of_vertices() << std::endl;
         }
+
         if (verbose >= 2) {
             os << std::endl
                 << std::setw(12) << "Loc. 1"
@@ -152,8 +153,12 @@ public:
                 << std::setw(12) << "------"
                 << std::setw(12) << "--------"
                 << std::endl;
-            for (VertexId vertex_id_1 = 0; vertex_id_1 < number_of_vertices(); ++vertex_id_1) {
-                for (VertexId vertex_id_2 = vertex_id_1 + 1; vertex_id_2 < number_of_vertices(); ++vertex_id_2) {
+            for (VertexId vertex_id_1 = 0;
+                    vertex_id_1 < number_of_vertices();
+                    ++vertex_id_1) {
+                for (VertexId vertex_id_2 = vertex_id_1 + 1;
+                        vertex_id_2 < number_of_vertices();
+                        ++vertex_id_2) {
                     os
                         << std::setw(12) << vertex_id_1
                         << std::setw(12) << vertex_id_2
@@ -162,6 +167,7 @@ public:
                 }
             }
         }
+
         return os;
     }
 
@@ -177,6 +183,16 @@ public:
                     "Unable to open file \"" + certificate_path + "\".");
         }
 
+        if (verbose >= 2) {
+            os << std::endl
+                << std::setw(12) << "Vertex"
+                << std::setw(12) << "Distance"
+                << std::endl
+                << std::setw(12) << "------"
+                << std::setw(12) << "--------"
+                << std::endl;
+        }
+
         VertexId vertex_id_pred = 0;
         VertexId vertex_id = -1;
         optimizationtools::IndexedSet vertices(number_of_vertices());
@@ -186,16 +202,19 @@ public:
         while (file >> vertex_id) {
             if (vertices.contains(vertex_id)) {
                 number_of_duplicates++;
-                if (verbose == 2)
+                if (verbose >= 2)
                     os << "Vertex " << vertex_id << " has already been visited." << std::endl;
             }
             vertices.add(vertex_id);
             total_distance += distance(vertex_id_pred, vertex_id);
-            if (verbose == 2)
-                os << "Vertex: " << vertex_id
-                    << "; Distance: " << distance(vertex_id_pred, vertex_id)
-                    << "; Total distance: " << total_distance
+
+            if (verbose >= 2) {
+                os
+                    << std::setw(12) << vertex_id
+                    << std::setw(12) << total_distance
                     << std::endl;
+            }
+
             vertex_id_pred = vertex_id;
         }
         total_distance += distance(vertex_id_pred, 0);
@@ -203,13 +222,15 @@ public:
         bool feasible
             = (vertices.size() == number_of_vertices())
             && (number_of_duplicates == 0);
-        if (verbose == 2)
+        if (verbose >= 2)
             os << std::endl;
         if (verbose >= 1) {
-            os << "Number of vertices:     " << vertices.size() << " / " << number_of_vertices()  << std::endl;
-            os << "Number of duplicates:   " << number_of_duplicates << std::endl;
-            os << "Feasible:               " << feasible << std::endl;
-            os << "Total distance:         " << total_distance << std::endl;
+            os
+                << "Number of vertices:     " << vertices.size() << " / " << number_of_vertices()  << std::endl
+                << "Number of duplicates:   " << number_of_duplicates << std::endl
+                << "Feasible:               " << feasible << std::endl
+                << "Total distance:         " << total_distance << std::endl
+                ;
         }
         return {feasible, total_distance};
     }
@@ -225,7 +246,6 @@ private:
     {
         std::string tmp;
         std::vector<std::string> line;
-        VertexId n = -1;
         std::string edge_weight_type;
         std::string edge_weight_format;
         std::string node_coord_type = "TWOD_COORDS";
@@ -237,7 +257,7 @@ private:
             } else if (tmp.rfind("TYPE", 0) == 0) {
             } else if (tmp.rfind("DISPLAY_DATA_TYPE", 0) == 0) {
             } else if (tmp.rfind("DIMENSION", 0) == 0) {
-                n = std::stol(line.back());
+                VertexId n = std::stol(line.back());
                 locations_ = std::vector<Location>(n);
                 distances_ = std::vector<std::vector<Distance>>(n, std::vector<Distance>(n, -1));
             } else if (tmp.rfind("EDGE_WEIGHT_TYPE", 0) == 0) {
@@ -249,42 +269,62 @@ private:
             } else if (tmp.rfind("EDGE_WEIGHT_SECTION", 0) == 0) {
                 if (edge_weight_format == "UPPER_ROW") {
                     Distance d;
-                    for (VertexId j1 = 0; j1 < n - 1; ++j1) {
-                        for (VertexId j2 = j1 + 1; j2 < n; ++j2) {
+                    for (VertexId vertex_id_1 = 0;
+                            vertex_id_1 < number_of_vertices() - 1;
+                            ++vertex_id_1) {
+                        for (VertexId vertex_id_2 = vertex_id_1 + 1;
+                                vertex_id_2 < number_of_vertices();
+                                ++vertex_id_2) {
                             file >> d;
-                            set_distance(j1, j2, d);
+                            set_distance(vertex_id_1, vertex_id_2, d);
                         }
                     }
                 } else if (edge_weight_format == "LOWER_ROW") {
                     Distance d;
-                    for (VertexId j1 = 1; j1 < n; ++j1) {
-                        for (VertexId j2 = 0; j2 < j1; ++j2) {
+                    for (VertexId vertex_id_1 = 1;
+                            vertex_id_1 < number_of_vertices();
+                            ++vertex_id_1) {
+                        for (VertexId vertex_id_2 = 0;
+                                vertex_id_2 < vertex_id_1;
+                                ++vertex_id_2) {
                             file >> d;
-                            set_distance(j1, j2, d);
+                            set_distance(vertex_id_1, vertex_id_2, d);
                         }
                     }
                 } else if (edge_weight_format == "UPPER_DIAG_ROW") {
                     Distance d;
-                    for (VertexId j1 = 0; j1 < n; ++j1) {
-                        for (VertexId j2 = j1; j2 < n; ++j2) {
+                    for (VertexId vertex_id_1 = 0;
+                            vertex_id_1 < number_of_vertices();
+                            ++vertex_id_1) {
+                        for (VertexId vertex_id_2 = vertex_id_1;
+                                vertex_id_2 < number_of_vertices();
+                                ++vertex_id_2) {
                             file >> d;
-                            set_distance(j1, j2, d);
+                            set_distance(vertex_id_1, vertex_id_2, d);
                         }
                     }
                 } else if (edge_weight_format == "LOWER_DIAG_ROW") {
                     Distance d;
-                    for (VertexId j1 = 0; j1 < n; ++j1) {
-                        for (VertexId j2 = 0; j2 <= j1; ++j2) {
+                    for (VertexId vertex_id_1 = 0;
+                            vertex_id_1 < number_of_vertices();
+                            ++vertex_id_1) {
+                        for (VertexId vertex_id_2 = 0;
+                                vertex_id_2 <= vertex_id_1;
+                                ++vertex_id_2) {
                             file >> d;
-                            set_distance(j1, j2, d);
+                            set_distance(vertex_id_1, vertex_id_2, d);
                         }
                     }
                 } else if (edge_weight_format == "FULL_MATRIX") {
                     Distance d;
-                    for (VertexId j1 = 0; j1 < n; ++j1) {
-                        for (VertexId j2 = 0; j2 < n; ++j2) {
+                    for (VertexId vertex_id_1 = 0;
+                            vertex_id_1 < number_of_vertices();
+                            ++vertex_id_1) {
+                        for (VertexId vertex_id_2 = 0;
+                                vertex_id_2 < number_of_vertices();
+                                ++vertex_id_2) {
                             file >> d;
-                            set_distance(j1, j2, d);
+                            set_distance(vertex_id_1, vertex_id_2, d);
                         }
                     }
                 } else {
@@ -294,24 +334,30 @@ private:
                 if (node_coord_type == "TWOD_COORDS") {
                     VertexId tmp;
                     double x, y;
-                    for (VertexId j = 0; j < n; ++j) {
+                    for (VertexId vertex_id = 0;
+                            vertex_id < number_of_vertices();
+                            ++vertex_id) {
                         file >> tmp >> x >> y;
-                        set_xy(j, x, y);
+                        set_xy(vertex_id, x, y);
                     }
                 } else if (node_coord_type == "THREED_COORDS") {
                     VertexId tmp;
                     double x, y, z;
-                    for (VertexId j = 0; j < n; ++j) {
+                    for (VertexId vertex_id = 0;
+                            vertex_id < number_of_vertices();
+                            ++vertex_id) {
                         file >> tmp >> x >> y >> z;
-                        set_xy(j, x, y, z);
+                        set_xy(vertex_id, x, y, z);
                     }
                 }
             } else if (tmp.rfind("DISPLAY_DATA_SECTION", 0) == 0) {
                 VertexId tmp;
                 double x, y;
-                for (VertexId j = 0; j < n; ++j) {
+                for (VertexId vertex_id = 0;
+                        vertex_id < number_of_vertices();
+                        ++vertex_id) {
                     file >> tmp >> x >> y;
-                    set_xy(j, x, y);
+                    set_xy(vertex_id, x, y);
                 }
             } else if (tmp.rfind("EOF", 0) == 0) {
                 break;
@@ -322,62 +368,82 @@ private:
 
         // Compute distances.
         if (edge_weight_type == "EUC_2D") {
-            for (VertexId j1 = 0; j1 < n; ++j1) {
-                for (VertexId j2 = j1 + 1; j2 < n; ++j2) {
-                    double xd = x(j2) - x(j1);
-                    double yd = y(j2) - y(j1);
+            for (VertexId vertex_id_1 = 0;
+                    vertex_id_1 < number_of_vertices();
+                    ++vertex_id_1) {
+                for (VertexId vertex_id_2 = vertex_id_1 + 1;
+                        vertex_id_2 < number_of_vertices();
+                        ++vertex_id_2) {
+                    double xd = x(vertex_id_2) - x(vertex_id_1);
+                    double yd = y(vertex_id_2) - y(vertex_id_1);
                     Distance d = std::round(std::sqrt(xd * xd + yd * yd));
-                    set_distance(j1, j2, d);
+                    set_distance(vertex_id_1, vertex_id_2, d);
                 }
             }
         } else if (edge_weight_type == "CEIL_2D") {
-            for (VertexId j1 = 0; j1 < n; ++j1) {
-                for (VertexId j2 = j1 + 1; j2 < n; ++j2) {
-                    double xd = x(j2) - x(j1);
-                    double yd = y(j2) - y(j1);
+            for (VertexId vertex_id_1 = 0;
+                    vertex_id_1 < number_of_vertices();
+                    ++vertex_id_1) {
+                for (VertexId vertex_id_2 = vertex_id_1 + 1;
+                        vertex_id_2 < number_of_vertices();
+                        ++vertex_id_2) {
+                    double xd = x(vertex_id_2) - x(vertex_id_1);
+                    double yd = y(vertex_id_2) - y(vertex_id_1);
                     Distance d = std::ceil(std::sqrt(xd * xd + yd * yd));
-                    set_distance(j1, j2, d);
+                    set_distance(vertex_id_1, vertex_id_2, d);
                 }
             }
         } else if (edge_weight_type == "GEO") {
-            std::vector<double> latitudes(n, 0);
-            std::vector<double> longitudes(n, 0);
-            for (VertexId j = 0; j < n; ++j) {
+            std::vector<double> latitudes(number_of_vertices(), 0);
+            std::vector<double> longitudes(number_of_vertices(), 0);
+            for (VertexId vertex_id = 0;
+                    vertex_id < number_of_vertices();
+                    ++vertex_id) {
                 double pi = 3.141592;
-                int deg_x = std::round(x(j));
-                double min_x = x(j) - deg_x;
-                latitudes[j] = pi * (deg_x + 5.0 * min_x / 3.0) / 180.0;
-                int deg_y = std::round(y(j));
-                double min_y = y(j) - deg_y;
-                longitudes[j] = pi * (deg_y + 5.0 * min_y / 3.0) / 180.0;
+                int deg_x = std::round(x(vertex_id));
+                double min_x = x(vertex_id) - deg_x;
+                latitudes[vertex_id] = pi * (deg_x + 5.0 * min_x / 3.0) / 180.0;
+                int deg_y = std::round(y(vertex_id));
+                double min_y = y(vertex_id) - deg_y;
+                longitudes[vertex_id] = pi * (deg_y + 5.0 * min_y / 3.0) / 180.0;
             }
             double rrr = 6378.388;
-            for (VertexId j1 = 0; j1 < n; ++j1) {
-                for (VertexId j2 = j1 + 1; j2 < n; ++j2) {
-                    double q1 = cos(longitudes[j1] - longitudes[j2]);
-                    double q2 = cos(latitudes[j1] - latitudes[j2]);
-                    double q3 = cos(latitudes[j1] + latitudes[j2]);
+            for (VertexId vertex_id_1 = 0;
+                    vertex_id_1 < number_of_vertices();
+                    ++vertex_id_1) {
+                for (VertexId vertex_id_2 = vertex_id_1 + 1;
+                        vertex_id_2 < number_of_vertices();
+                        ++vertex_id_2) {
+                    double q1 = cos(longitudes[vertex_id_1] - longitudes[vertex_id_2]);
+                    double q2 = cos(latitudes[vertex_id_1] - latitudes[vertex_id_2]);
+                    double q3 = cos(latitudes[vertex_id_1] + latitudes[vertex_id_2]);
                     Distance d = (Distance)(rrr * acos(0.5 * ((1.0 + q1) * q2 - (1.0 - q1) * q3)) + 1.0);
-                    set_distance(j1, j2, d);
+                    set_distance(vertex_id_1, vertex_id_2, d);
                 }
             }
         } else if (edge_weight_type == "ATT") {
-            for (VertexId j1 = 0; j1 < n; ++j1) {
-                for (VertexId j2 = j1 + 1; j2 < n; ++j2) {
-                    double xd = x(j1) - x(j2);
-                    double yd = y(j1) - y(j2);
+            for (VertexId vertex_id_1 = 0;
+                    vertex_id_1 < number_of_vertices();
+                    ++vertex_id_1) {
+                for (VertexId vertex_id_2 = vertex_id_1 + 1;
+                        vertex_id_2 < number_of_vertices();
+                        ++vertex_id_2) {
+                    double xd = x(vertex_id_1) - x(vertex_id_2);
+                    double yd = y(vertex_id_1) - y(vertex_id_2);
                     double rij = sqrt((xd * xd + yd * yd) / 10.0);
                     int tij = std::round(rij);
                     Distance d = (tij < rij)? tij + 1: tij;
-                    set_distance(j1, j2, d);
+                    set_distance(vertex_id_1, vertex_id_2, d);
                 }
             }
         } else if (edge_weight_type == "EXPLICIT") {
         } else {
             std::cerr << "\033[31m" << "ERROR, EDGE_WEIGHT_TYPE \"" << edge_weight_type << "\" not implemented." << "\033[0m" << std::endl;
         }
-        for (VertexId j = 0; j < n; ++j)
-            distances_[j][j] = std::numeric_limits<Distance>::max();
+        for (VertexId vertex_id = 0;
+                vertex_id < number_of_vertices();
+                ++vertex_id)
+            distances_[vertex_id][vertex_id] = std::numeric_limits<Distance>::max();
     }
 
     /*
@@ -389,6 +455,10 @@ private:
 
     /** Distances between locations. */
     std::vector<std::vector<Distance>> distances_;
+
+    /*
+     * Computed attributes
+     */
 
     /** Maximum distance. */
     Distance distance_max_ = 0;
